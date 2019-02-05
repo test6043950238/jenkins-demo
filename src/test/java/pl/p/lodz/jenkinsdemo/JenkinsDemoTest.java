64ba884b5fcef7ee9fa85896a1d2b70f31c16819
test604395002@gmail.com <test604395002@gmail.com>
2019-02-05 14:20:11 +0100

    adding redundant code for jenkins' features

--- src/test/java/pl/p/lodz/jenkinsdemo/JenkinsDemoTest.java
+++ src/test/java/pl/p/lodz/jenkinsdemo/JenkinsDemoTest.java
@@ -14,21 +14,21 @@ class JenkinsDemoTest{
     @DisplayName("Test Demo with number: 1")
     @Test
     public void testNumberOne() {
-        String demoString = jd.test(1);
+        String demoString = jd.demo(1);
         Assertions.assertEquals(demoString, "1");
     }
 
     @DisplayName("Test Demo with number: 2")
     @Test
     public void testNumberTwo() {
-        String demoString = jd.test(2);
+        String demoString = jd.demo(2);
         Assertions.assertEquals(demoString, "It's a two");
     }
 
     @DisplayName("Test Demo with number: 4")
     @Test
     public void testNumberFour() {
-        String demoString = jd.test(4);
+        String demoString = jd.demo(4);
         Assertions.assertEquals(demoString, "It's a four");
     }
 
