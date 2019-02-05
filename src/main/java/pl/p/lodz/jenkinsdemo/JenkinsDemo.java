4ac5339b850e5d864caefe34f747ad772d1fd927
test604395002@gmail.com <test604395002@gmail.com>
2019-02-05 13:50:38 +0100

    adding redundant code for warnings

--- src/main/java/pl/p/lodz/jenkinsdemo/JenkinsDemo.java
+++ src/main/java/pl/p/lodz/jenkinsdemo/JenkinsDemo.java
@@ -3,6 +3,7 @@ package pl.p.lodz.jenkinsdemo;
 public class JenkinsDemo{
 
     public String demo(int input){
+        int unnecessaryvalue;
         if(input == -1){
             throw new IllegalArgumentException("Number must not be -1");
         }
